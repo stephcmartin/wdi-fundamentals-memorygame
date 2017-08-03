39a71b35cc927d0fcfa6593600dcd1b94f497635
Stephanie Martin <martin.stephaniechow@gmail.com>
2017-08-03 16:49:01 -0400

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-console.log("Up and running!");
-
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
